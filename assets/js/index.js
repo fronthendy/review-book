3fa7a6f4263d9218bfd75a20bf8084616c2b5b9f
fronthendy <hendy.almeida@hotmail.com>
2019-08-30 19:09:33 -0300

    modal dinamico, cards dinamocos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,53 @@
-console.log('arquivo externo');
-
-let livros = [
-    {
+let livros = [{
         nome: '1984',
         imagem: 'assets/img/1984-poster.jpg',
-        nota: 5  
+        nota: 2
     },
     {
         nome: 'O menino do pijama listrado',
         imagem: 'assets/img/pijama-poster.jpg',
-        nota: 4 
+        nota: 4
     },
     {
         nome: 'Fortaleza Digital',
         imagem: 'assets/img/fortaleza-poster.jpg',
-        nota: 4 
+        nota: 4
     }
 ];
 
 let sectionLivros = document.querySelector('.livros .container');
 
-for (let livro of livros) {
-    sectionLivros.append(`nome: ${livro.nome} | nota: ${livro.nota} --`)
+let htmlLivros = "";
+
+livros.forEach((livro, id) => {
+    htmlLivros += `
+    <article>
+        <div onclick="abrirModal(${id})" class="img-poster" style="background-image: url(${livro.imagem})"></div>
+        <div class="descricao">
+            <h1><a onclick="abrirModal(${id})">${livro.nome}</a></h1>
+            <div class="stars">
+                ${'<span class="star-filled"></span>'.repeat(livro.nota)}
+                ${'<span class="star-outline"></span>'.repeat(5 - livro.nota)}
+            </div>
+        </div>
+    </article>
+`
+})
+
+sectionLivros.innerHTML = htmlLivros;
+
+let body = document.querySelector('body');
+let modal = document.querySelector('.modal');
+let modalTitulo = document.querySelector('.modal .titulo');
+let modalPoster = document.querySelector('.modal .poster');
+
+
+function abrirModal(id){
+        modalTitulo.innerText = livros[id].nome
+        modalPoster.src = livros[id].imagem
+        body.classList.add('modal-ativo');
 }
 
-
+function fecharModal(){
+    body.classList.remove('modal-ativo')
+}
