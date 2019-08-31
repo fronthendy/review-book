b6bb3b740c8ac52fbae6ca680ce27a2e4dbae500
fronthendy <hendy.almeida@hotmail.com>
2019-08-30 21:54:36 -0300

    livros exibidos dinamicamente + modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,13 +15,51 @@ let livros = [
         nome: 'Fortaleza Digital',
         imagem: 'assets/img/fortaleza-poster.jpg',
         nota: 4 
+    },
+    {
+        nome: 'Deserto dos Tártaros',
+        imagem: 'assets/img/deserto-poster.jpg',
+        nota: 2
+
     }
 ];
 
 let sectionLivros = document.querySelector('.livros .container');
 
-for (let livro of livros) {
-    sectionLivros.append(`nome: ${livro.nome} | nota: ${livro.nota} --`)
+let htmlLivros = "";
+
+livros.forEach((livro, id) => {
+    htmlLivros += `<article>
+    <div  onclick="abrirModal(${id})" class="img-poster" style="background-image: url(${livro.imagem})"></div>
+    <div class="descricao">
+        <h1><a href="#" onclick="abrirModal(${id})">${livro.nome}</a></h1>
+        <div class="stars" onclick="abrirModal(${id})">
+            ${'<span class="star-filled"></span>'.repeat(livro.nota)}
+            ${'<span class="star-outline"></span>'.repeat(5 - livro.nota)}
+        </div>
+    </div>
+</article>`;
+})
+
+sectionLivros.innerHTML = htmlLivros;
+
+let modalTitulo = document.querySelector('.modal .titulo');
+let modalPoster = document.querySelector('.modal .poster');
+
+function abrirModal(id){
+    modalTitulo.innerText = livros[id].nome;
+    modalPoster.src = livros[id].imagem;
+    document.querySelector('body').classList.add('modal-ativo');
 }
 
+function fecharModal(){
+    document.querySelector('body').classList.remove('modal-ativo');
+}
 
+document.addEventListener('keydown', function(evento){
+    console.log(evento.keyCode);
+    
+    if(evento.keyCode == 27){
+        fecharModal();
+    }
+});
